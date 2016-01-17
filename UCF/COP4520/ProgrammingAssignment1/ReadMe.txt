0a1c80eb88db5c0a1ee9074a458469733606d0e3
trougnouf <trougnouf@gmail.com>
2016-01-17 17:56:07 -0500

    COP4520 P1: This produces an erroneous result. About to break everything.

--- UCF/COP4520/ProgrammingAssignment1/ReadMe.txt
+++ UCF/COP4520/ProgrammingAssignment1/ReadMe.txt
@@ -18,4 +18,14 @@ The addition of any number of threads (MAXTHREADS) can safely be implemented wit
 A structure containing the variables "curValue" and "isWaiting" is created for each thread. It is passed from the main thread as a pointer, allowing the main thread to easily modify these values.
 Each thread runs an infinity loop that ends when "curValue" is set to zero. Whenever "isWaiting" is set to false, the thread sets the "isComposite" boolean of every multiple of "curValue" to true, then switches the "isWaiting" flag back to true. The main thread looks for waiting threads, gives them a new "curValue" (using the nextPrime function), and resets the "isWaiting" flag to 1.
 
+Benchmarking:
+Using MAXNUM 100000001 (10^8, excluding 0)
+
+Using 8 threads:
+gcc -lpthread  problem1.c && ./a.out
+Execution time: 5.111911s
+Found 6260293 primes.
+Sum of primes = 2387434306
+Top ten maximum primes: 99999787, 99999821, 99999827, 99999839, 99999847, 99999931, 99999941, 99999959, 99999971, 99999989
+
 
--- UCF/COP4520/ProgrammingAssignment1/problem1.c
+++ UCF/COP4520/ProgrammingAssignment1/problem1.c
@@ -2,13 +2,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
-#include <time.h>
+#include <sys/time.h>
 #define MAXNUM 100000001
-#define MAXTHREADS 8
+#define MAXTHREADS 1
 
-void* thread_compositeFinder(void* args);
-uint8_t * isComposite;
-uint32_t nextPrime(uint32_t curPrime);
+void* thread_compositeFinder(void* args);	// thread
+
+volatile uint8_t * isComposite;				// global array
+
+						// function prototypes
+uint32_t nextPrime(uint32_t * curPrime);		
+void printResults(struct timeval * begTime);
+double getTimeElapsed(struct timeval * begTime);
 
 struct fiveBytes {
 	volatile uint32_t curValue;
@@ -17,7 +22,8 @@ struct fiveBytes {
 
 int main()
 {
-	clock_t begTime = clock();
+	struct timeval begTime;
+	gettimeofday(&begTime, NULL);
 	uint32_t curPrime = 1;
 	// initialize empty array of 10^8 characters, threads, and their data
 	isComposite = (uint8_t*) calloc(MAXNUM, 1);
@@ -26,13 +32,13 @@ int main()
 	volatile struct fiveBytes threadData[MAXTHREADS];
 	for(uint8_t i=0; i<MAXTHREADS; i++)
 	{
-		curPrime = nextPrime(curPrime);
+		curPrime = nextPrime(&curPrime);
 		threadData[i].isWaiting = 0;
 		threadData[i].curValue = curPrime;
 		pthread_create(	&threads[i], NULL, thread_compositeFinder,
 				(void *) &threadData[i]);
 	}	
-	curPrime = nextPrime(curPrime);
+	curPrime = nextPrime(&curPrime);
 	// loop over each thread, give out work when necessary
 	while(curPrime)
 	{
@@ -42,18 +48,25 @@ int main()
 			{
 				threadData[i].curValue = curPrime;
 				threadData[i].isWaiting = 0;
-				curPrime = nextPrime(curPrime);
+				curPrime = nextPrime(&curPrime);
 			}
 		}
 	}
 	
 	// make all threads commit suicide
-	for(uint8_t i = 0; i < MAXTHREADS; i++)	threadData[i].curValue = 0;
-	for(uint8_t i = 0; i < MAXTHREADS; i++)	pthread_join(threads[i], NULL);
+	for(uint8_t i = 0; i < MAXTHREADS; i++)
+	{
+		threadData[i].curValue = 0;
+		pthread_join(threads[i], NULL);
+	}
 	
-	printResults(begTime);
+	printResults(&begTime);
 
-	//for(uint32_t i=0; i<MAXNUM; i++)	printf("%u:\t%u\n", i, isComposite[i]);
+	/*
+	// uncomment this to get the complete list of prime numbers
+	for(uint32_t i=0; i<MAXNUM; i++)
+		printf("%u:\t%u\n", i, isComposite[i]);
+	*/
 }
 
 void* thread_compositeFinder(void* args)
@@ -76,21 +89,47 @@ void* thread_compositeFinder(void* args)
 	}
 }
 
-uint32_t nextPrime(uint32_t curPrime)
+uint32_t nextPrime(uint32_t * curPrime)
 {
-	if(!curPrime)	return 0;
-	for(uint32_t i = curPrime+1; curPrime < MAXNUM; i++)
+	if(!(*curPrime))	return 0;
+	for(uint32_t i = *curPrime+1; *curPrime < MAXNUM; i++)
 		if(!isComposite[i])	return i;
 	return 0;
 }
 
-void printResults()
+void printResults(struct timeval * begTime)
 {
-	printf();
-	
+	uint32_t numOfPrimes = 0;
+	uint64_t sumOfPrimes = 0;
+	uint32_t maxPrimes[10];
+	/*
+	// it takes approximately 0.30s to analyze the results on a single thread
+	printf(	"Execution time: %fs (excluding print statement)\n",
+		getTimeElapsed(begTime));
+	*/
+	for(uint32_t i = MAXNUM-1; i > 1; i--)
+	{
+		if(!isComposite[i])
+			{
+				if(numOfPrimes < 10)
+					maxPrimes[numOfPrimes] = i;
+				numOfPrimes++;
+				sumOfPrimes += i;
+			}
+	}
+	printf(	"Execution time: %fs\n",
+		getTimeElapsed(begTime));
+	printf("Found %u primes.\n", numOfPrimes);
+	printf("Sum of primes = %u\n", sumOfPrimes);
+	printf("Top ten maximum primes: ");
+	for(int8_t i = 9; i >= 0; i--)
+		printf("%u%s", maxPrimes[i], i?", ":"\n");
 }
 
-/*
- * Benchmark (on Thinkpad X200t old laptop):
- * Single thread, no optimization: 6976B, 44s (~2% dif w/"dynamic programming", 
- */
+double getTimeElapsed(struct timeval * begTime)
+{
+	struct timeval curTime;
+	gettimeofday(&curTime, NULL);
+	return (double)	((curTime.tv_usec - (*begTime).tv_usec)/1000000. +
+			curTime.tv_sec - (*begTime).tv_sec);
+}
